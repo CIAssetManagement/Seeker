489e640a7760fd73c6f16ec06d3114412982a688
Mauricio <MauricioTrejo@users.noreply.github.com>
2017-12-01 12:16:53 -0600

    ...

--- Transferencia.R
+++ Transferencia.R
@@ -1,7 +1,7 @@
 library(rdrop2)
 
 #Copiando el archivo de Fondos
-drop_upload(file = "C:/Github/Funds/Fondos.xlsx",path = 'Carpeta del equipo CIEstrategias',
+drop_upload(file = "C:/Github/Funds/Fondos.csv",path = 'Carpeta del equipo CIEstrategias',
             mode = "overwrite",verbose = TRUE)
 
 #Copiando el archivo de instrumentos
